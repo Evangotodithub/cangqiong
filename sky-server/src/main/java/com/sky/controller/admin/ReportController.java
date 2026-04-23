77a31339e94cf9cc71335d3bd3bdff35b273561a
Evangotodithub <liaoxin02270227@163.com>
2026-04-23 21:33:40 +0900

    2026.04.23

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -1,4 +1,12 @@
 package com.sky.controller.admin;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/report")
+@Slf4j
+
 public class ReportController {
 }
