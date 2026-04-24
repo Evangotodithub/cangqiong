c7ad6b86fcf94bace81a1ed3065d2e9471d0bba4
Evangotodithub <liaoxin02270227@163.com>
2026-04-24 17:35:52 +0900

    2026.04.23

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/report")
+@RequestMapping("/admin/report")
 @Slf4j
 
 public class ReportController {
