805ade23206ad17a9f6d81dbd93a2e472b19c1a1
Evangotodithub <liaoxin02270227@163.com>
2026-04-24 18:37:23 +0900

    2026.04.24

---
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -0,0 +1,12 @@
+package com.sky.mapper;
+
+import com.github.pagehelper.Page;
+import com.sky.dto.OrdersPageQueryDTO;
+import com.sky.entity.Orders;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+
+public interface OrderMapper {
+    Page<Orders> pageQuery(OrdersPageQueryDTO ordersPageQueryDTO);
+}
--- sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -1,16 +1,60 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.dto.OrdersPageQueryDTO;
+import com.sky.entity.OrderDetail;
+import com.sky.entity.Orders;
+import com.sky.mapper.OrderDetailMapper;
+import com.sky.mapper.OrderMapper;
 import com.sky.result.PageResult;
 import com.sky.service.OrderService;
+import com.sky.vo.OrderVO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
 public class OrderServiceImpl implements OrderService {
+    @Autowired
+    private OrderMapper orderMapper;
+    @Autowired
+    private OrderDetailMapper orderDetailMapper;
     @Override
     public PageResult conditionQuery(OrdersPageQueryDTO ordersPageQueryDTO) {
-        return null;
+        PageHelper.startPage(ordersPageQueryDTO.getPage(),ordersPageQueryDTO.getPageSize());
+        Page<Orders> page = orderMapper.pageQuery(ordersPageQueryDTO);
+        List<OrderVO> orderVOList = getOrderVOList(page);
+        return new PageResult(page.getTotal(),orderVOList);
+    }
+
+    private List<OrderVO> getOrderVOList(Page<Orders> page) {
+        List<OrderVO> orderVOList = new ArrayList<>();
+        List<Orders> orderList = page.getResult();
+        if(orderList != null && orderList.size()>0) {
+            for (Orders orders : orderList) {
+                OrderVO orderVO = new OrderVO();
+                BeanUtils.copyProperties(orders, orderVO);
+                String orderDishes = getOrderDishesStr(orders);
+                orderVO.setOrderDishes(orderDishes);
+                orderVOList.add(orderVO);
+            }
+        }
+        return orderVOList;
+    }
+
+    private String getOrderDishesStr(Orders orders) {
+        List<OrderDetail> orderDetailList = orderDetailMapper.getById(orders.getId());
+        List<String> orderDishList = orderDetailList.stream().map(x->{
+            String orderDish = x.getName() + "*" + x.getNumber()+ ";";
+            return orderDish;
+        }).collect(Collectors.toList());
+        return String.join("",orderDishList);
     }
 }
